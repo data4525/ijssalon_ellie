1750e21e85a97384e3dee8310dcae2212f97f4f2
wpari <wpari17@live.nl>
2023-06-19 12:48:23 +0200

    first commit

--- tijdelijk.py
+++ tijdelijk.py
@@ -1,2 +1,5 @@
-aanbieding_aardbei = {"aardbei" : "*0.8"}
-print (aanbieding_aardbei)
+input ("Welk getal wilt u delen?")
+aanbieding_1 = 3
+aanbieding_2 = 0.8
+totaal = aanbieding_1 + aanbieding_2
+print (totaal)
