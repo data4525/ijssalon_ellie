094fdd4e7cfa37329da96b250d046b71862bccf4
wpari <wpari17@live.nl>
2023-06-21 13:40:52 +0200

    first commit

--- tijdelijk.py
+++ tijdelijk.py
@@ -1,3 +1,2 @@
-reclame_tekst = ["Vandaag in de aanbieding: vanille-ijs, 1 liter-slechts €<2>"]
-reclame_tekst2 = reclame_tekst [:2]
-print (reclame_tekst2) [:2]
+reclame_tekst = "Vandaag in de aanbieding: vanille-ijs, 1 liter-slechts € <2 euro>"
+print(reclame_tekst)
