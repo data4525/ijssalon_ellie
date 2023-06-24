d4a95c13b87be1ff5f3d5894aba25f8bbedab1ab
wpari <wpari17@live.nl>
2023-06-24 12:32:21 +0200

    first commit

--- tijdelijk.py
+++ tijdelijk.py
@@ -1,3 +1,12 @@
-reclame_tekst = f"Vandaag in de aanbieding: vanille-ijs, 1 liter-slechts € {aanbieding}"
+prijzen = {
+    "aardbei" : 3,
+    "vanille" : 4,
+    "chocolade" : 5
+}
+
+aanbieding = prijzen ["aardbei"] * 0.8
+
+reclame_tekst = f"Vandaag in de aanbieding: Vanille-ijs, 1 Liter - slechts € {aanbieding}"
+
 reclame_tekst2 = reclame_tekst[:63]
 print(reclame_tekst2)
