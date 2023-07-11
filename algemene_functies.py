13c70cd9d53ca5cd6aaa23604808b7bc585f3d5d
wpari <wpari17@live.nl>
2023-07-11 11:47:57 +0200

    first commit

--- algemene_functies.py
+++ algemene_functies.py
@@ -1,3 +1,17 @@
 def mijn_functie_1(x):
+    return 2*x
+print(mijn_functie_1(2))
+
+def mijn_functie_1(x):
     return 4*x
 print(mijn_functie_1(4))
+
+def mijn_functie_1(x):
+    return 10*x
+print(mijn_functie_1(10))
+
+def mijn_functie_1(x):
+    return 12*x
+print(mijn_functie_1(12))
+
+
