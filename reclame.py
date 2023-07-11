9233b17209aa7923e984005410293735ccbed5af
wpari <wpari17@live.nl>
2023-07-11 12:09:44 +0200

    first commit

---
+++ reclame.py
@@ -0,0 +1,8 @@
+def aanbieding_1(smaak, prijs, korting):
+    print("Vandaag in de aanbieding: emmertje ijs (1 liter)", korting)
+    print("in de smaak aardbei,",smaak)
+    print("van 4 euro voor 3.60 euro.", prijs)
+    
+    return ("aardbei","4","0.1")
+
+aanbieding_1("","","")
