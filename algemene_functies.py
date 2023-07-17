1b347d5b927600e8dafb30886a94f1ab56c02cea
WS <wpari17@live.nl>
2023-07-17 17:34:35 +0200

    first commit

--- algemene_functies.py
+++ algemene_functies.py
@@ -1,3 +1,14 @@
 def mijn_functie_1(a):
     return a * a
 print (mijn_functie_1(2))
+print (mijn_functie_1(4))
+print (mijn_functie_1(10))
+print (mijn_functie_1(12))
+
+def mijn_functie_2(a,b):
+    uitvoer_lijst = [12.3]
+    uitvoer_lijst.append(a+b)
+    uitvoer_lijst.append(a-b)
+    uitvoer_lijst.append(a*b)
+    uitvoer_lijst.append(a/b)
+    return uitvoer_lijst
