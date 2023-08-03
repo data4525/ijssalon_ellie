ca7a4ed181178747f825142b9f6e62968d885802
data4525 <136325586+data4525@users.noreply.github.com>
2023-08-03 12:15:52 +0200

    Add files via upload

---
+++ algemene_functies.py
@@ -0,0 +1,28 @@
+def mijn_functie_1(a):
+    return a * a
+som = mijn_functie_1(2)
+print(som)
+
+def mijn_functie_2(a,b):
+    uitvoer_lijst = []
+    uitvoer_lijst.append(a+b)
+    uitvoer_lijst.append(a-b)
+    uitvoer_lijst.append(a*b)
+    uitvoer_lijst.append(a/b)
+    return uitvoer_lijst
+
+som = mijn_functie_2(12,2)
+print(som)
+
+
+    
+
+
+
+
+
+
+
+
+
+
