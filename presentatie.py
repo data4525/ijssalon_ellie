10de4c6f7eda5ebd2247f7382b8accb024e76885
data4525 <136325586+data4525@users.noreply.github.com>
2023-09-12 12:59:38 +0200

    Update presentatie.py

--- presentatie.py
+++ presentatie.py
@@ -1,13 +1,9 @@
-def presenteer (dictionary, totaal):
-    for item, amount in dictionary.items():
-        print(f"{item} : {amount} euro")
-      
-mijn_dict = {'vis' : 10, 'vlees': 25, 'overig': 15}
-totaal = 50
-presenteer (mijn_dict, totaal)
+def presenteer (dict, totaal):
+    for key, in dict:
+        print(key, ':', dict[key], 'euro')
 
 print("============================")
-print(f"Totaal : {50} euro")    
+print("Totaal:', totaal, 'euro')  
 
 
 
