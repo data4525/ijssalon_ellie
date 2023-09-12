cec55666b37d45ba7283ef9fc82d6e02080d7d22
data4525 <136325586+data4525@users.noreply.github.com>
2023-09-12 13:08:07 +0200

    Update boekhouding.py

--- boekhouding.py
+++ boekhouding.py
@@ -1,23 +1,21 @@
-import helper
 import csv
-from helper import som
-from presentatie import presenteer
+from helper import *
+from presentatie import *
+
 
 inkomsten = {
-    'Aardbeien-ijs-totaal': 1000,
-    'Vanille-ijs-totaal': 2000,
-    'Chocolade-ijs-totaal': 1500,
-    'Waterijsjes-totaal': 750
+    "Aardbeien-ijs-totaal": 1000,
+    "Vanille-ijs-totaal": 2000,
+    "Chocolade-ijs-totaal": 1500,
+    "Waterijsjes-totaal": 750
 }
 
 totaal_inkomsten = som(inkomsten)
+
 presenteer (inkomsten, totaal_inkomsten)
 
-print("============================")
-print (f"Totaal: {totaal_inkomsten} euro")
-
 with open ('boekhouding.csv','w',newline='') as csvfile:
-                for key, value in inkomsten.items():
+    for key, value in inkomsten.items():
                             writer = csv.writer(csvfile,delimiter=';')
                             writer.writerow ([key,value])
 
