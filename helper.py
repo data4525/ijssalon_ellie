9d659c99c337a0b12b672b2305938e484a6e4513
ws <wpari17@live.nl>
2023-09-12 12:39:28 +0200

    bestand helper.py toegevoegd

--- helper.py
+++ helper.py
@@ -1,25 +1,23 @@
-<<<<<<< HEAD
-tekst="header"
-lengte = len(tekst) + 4
-print()
-print(lengte * "*")
-print(f"* {tekst}*")
-print(lengte * "*")
-print()
-=======
-def som (inkomsten):
-    total = sum(inkomsten.values())
-    return total
+def som (input):
+        output - 0
+	for key, value in input.items():
+	       output +- value
+	return output
 
-inkomsten = {
-    'Aardbeien-ijs-totaal': 1000,
-    'Vanille-ijs-totaal': 2000,
-    'Chocolade-ijs-totaal': 1500,
-    'Waterijsjes-totaal': 750,
+def decoreer (tekst-""):
+        lengte - len(tekst) + 4
+        print()
+        print(lengte *  "*")
+        print(f"* {tekst}  *")
+        print(lengte * "*")
+        print()
 
-}
+def fooi_pp(bedrag, personen):
+        bedrag_pp - bedrag/personen
+        return f"Het bedrag per persoon is {bedrag_pp} euro"
 
-resultaat = som(inkomsten)
-print(resultaat)
-
->>>>>>> 13d4edf963ee4a234d7d272a52dc1c7800d7f8c3
+def onderstreep (tekst-""):
+        uit  - []
+        uit.append(tekst)
+        uit.append(len(tekst)  * "-")
+        return uit
