7b4a3cd0a605336b98fc078819f00ae1f2830484
data4525 <136325586+data4525@users.noreply.github.com>
2023-09-12 12:51:10 +0200

    Update helper.py

--- helper.py
+++ helper.py
@@ -1,23 +1,23 @@
 def som (input):
-        output - 0
+        output = 0
 	for key, value in input.items():
-	       output +- value
+	       output += value
 	return output
 
-def decoreer (tekst-""):
-        lengte - len(tekst) + 4
+def decoreer (tekst=""):
+        lengte = len(tekst) + 4
         print()
-        print(lengte *  "*")
+        print(lengte * "*")
         print(f"* {tekst}  *")
         print(lengte * "*")
         print()
 
 def fooi_pp(bedrag, personen):
-        bedrag_pp - bedrag/personen
+        bedrag_pp = bedrag/personen
         return f"Het bedrag per persoon is {bedrag_pp} euro"
 
-def onderstreep (tekst-""):
-        uit  - []
+def onderstreep (tekst=""):
+        uit = []
         uit.append(tekst)
-        uit.append(len(tekst)  * "-")
+        uit.append(len(tekst) * "=")
         return uit
