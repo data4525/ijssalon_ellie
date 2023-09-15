1f9bd5c125d05b38f5fa904ea92b2f0b4285396e
data4525 <136325586+data4525@users.noreply.github.com>
2023-09-15 11:28:00 +0200

    Update algemene_functies.py

--- algemene_functies.py
+++ algemene_functies.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 def mijn_functie_1(a):
     return a * a
 som = mijn_functie_1(2)
@@ -27,33 +26,4 @@ print(som)
 
 
 
-=======
-def mijn_functie_1(a):
-    return a * a
-som = mijn_functie_1(2)
-print(som)
 
-def mijn_functie_2(a,b):
-    uitvoer_lijst = []
-    uitvoer_lijst.append(a+b)
-    uitvoer_lijst.append(a-b)
-    uitvoer_lijst.append(a*b)
-    uitvoer_lijst.append(a/b)
-    return uitvoer_lijst
-
-som = mijn_functie_2(12,2)
-print(som)
-
-
-    
-
-
-
-
-
-
-
-
-
-
->>>>>>> 13d4edf963ee4a234d7d272a52dc1c7800d7f8c3
